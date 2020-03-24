5e9b50bc43ecaea93e580a0bfcd30b69bc03352b
Sonja Parsell <hello@sparsell.com>
2020-03-24 10:49:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,12 @@ def countdown(seconds)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1.0)
+  end
+  return "HAPPY NEW YEAR!"
+end
 end
