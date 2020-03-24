a3398f401f185037f9566e116582b24e4f628e2b
Sonja Parsell <hello@sparsell.com>
2020-03-24 10:35:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown
-  counter == 100
-  while counter > 0
-    puts "#${seconds} SECOND(S)!"
-    counter -= 1
+  seconds == 100
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
   end
 
   puts "HAPPY NEW YEAR!"
